32a2f07ce47f8396f1ad6b9098cb9dc2161ff247
SteadfastAsArt <695488173@qq.com>
2019-09-12 17:07:58 +0800

    Update Knapsack.cpp

--- Dynamic-Programming/Knapsack.cpp
+++ Dynamic-Programming/Knapsack.cpp
@@ -1,7 +1,15 @@
 /* 
+ * Knapsack Problem:
+ * ------------------------
+ * Definition: 
+ * Given a set of n items, each with a weight w_i and a value v_i, 
+ * determine the number of each item to include in a collection so that 
+ * the total weight is less than or equal to a given limit W and the total 
+ * value is as large as possible.
  * 
- * 
- * 
+ * Solving:
+ * Brute force: O(2^n)
+ * DP: O(n*W)
  * 
  * */
 
@@ -12,6 +20,7 @@ using namespace std;
 #define push_back pb
 
 
+// K[n][c]: maximum value achieved weight <= c using items up to n
 int KnapSack(int num_item, int C, vector<int>w, vector<int>v ){
     int K[num_item+1][C+1];
     memset(K, 0, sizeof(int) * ((num_item+1)*(C+1)) );
@@ -19,7 +28,10 @@ int KnapSack(int num_item, int C, vector<int>w, vector<int>v ){
     {
         for(int c = 1; c <= C; c++)
         {
-            K[n][c] = w[i-1] <= c ? max(K[n-1][ c - v[i-1] ] + v[i-1], K[n-1][c]) : K[n-1][c];
+            // With certain number of items n and certain capacity c,
+            // if it can be placed, compare the value between pack / non-pack,
+            // else inherit from the n-1 item status (non-pack that is)
+            K[n][c] = w[n-1] <= c ? max(K[n-1][ c - v[n-1] ] + v[n-1], K[n-1][c]) : K[n-1][c];
         }
     }
 
@@ -44,7 +56,7 @@ int main(){
         w.pb(tmp);
     }
 
-    int ans = KnapSack()
+    int ans = KnapSack(num_item, C, w, v);
 
 
     return 0;
