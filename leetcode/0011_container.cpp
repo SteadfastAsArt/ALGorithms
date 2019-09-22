a511c654803b43f3a764c78a0b2f2046d712ecad
SteadfastAsArt <695488173@qq.com>
2019-09-22 23:28:00 +0800

    create leetcode directory

---
+++ leetcode/0011_container.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int maxn = 1e3;
+
+/*
+ * this problem can be solved in O(N)
+ * two pointers
+ * 
+ */
+
+int maxArea(vector<int>& height) {
+    int v = 0;
+    for(int i = 0; i < height.size(); i++)
+        for(int j = 0; j < height.size(); j++)
+        {
+            if(i < j)
+            {
+                int v_now = min(height[i], height[j]) * (j - i);
+                cout << v_now << " " << endl;
+                if(v < v_now) v = v_now;
+            }
+
+        }
+    return v;
+}
+
+int main(){
+    int arr[9] = {1, 8, 6, 2, 5, 4, 1, 3, 7};
+    vector<int> h(arr, arr+9);
+    int res = maxArea(h);
+    cout << res;
+
+
+
+    return 0;
+}
+
+/*
+int maxArea(vector<int> & height)
+{
+    int res = 0;
+    for (int b = 0, e = height.size()-1; b < e; )
+        res = std::max(res, (e-b) * (height[b] < height[e] ? height[b++] : height[e--]));
+    return res;
+}
+*/
