3e3c27541efebdb935bf6eda4361f87169656125
SteadfastAsArt <695488173@qq.com>
2019-10-05 23:35:04 +0800

    Create 0046_permutations.cpp

---
+++ leetcode/0046_permutations.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/**
+ * 0046 - Given a list of numbers, return all of the permutations
+ * === ===
+ * Backtracking
+ */
+
+vector<vector<int> > permute(vector<int>& nums) {
+    vector<vector<int> > res;
+    dfs(nums, 0, res);
+    return res;
+}
+
+// permute: num[begin..end]
+// invariant: num[0..begin-1] have been permuted & fixed
+void dfs(vector<int> &nums, int begin, vector<vector<int> > &result)
+{
+    if(begin >= nums.size())
+    {
+        result.push_back(nums);
+        return;
+    }
+
+    for(int i = begin; i < nums.size(); i++)
+    {
+        swap(nums[begin], nums[i]);
+        dfs(nums, begin + 1, result);
+        swap(nums[begin], nums[i]);  /* reset */
+    }
+}
