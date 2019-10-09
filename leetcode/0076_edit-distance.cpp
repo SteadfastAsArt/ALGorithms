4288e027247b428de98b9091ebe1a81fd81081c0
SteadfastAsArt <695488173@qq.com>
2019-10-09 23:59:32 +0800

    Create 0076_edit-distance.cpp

---
+++ leetcode/0076_edit-distance.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+/*
+ * 0076 - Given two strings, there are there ways to operate on 
+ * the first one to make it identical to the second one, which 
+ * are "insert", "delete", "replace".
+ * === ===
+ * DP
+ * the state dp[i][j] to be the minimum number of operations 
+ * to convert word1[0..i) to word2[0..j)
+ */
+
+int minDistance(string word1, string word2) {
+
+}
+
